3e350144ff9a04882244313057db6d716d74a01f
sri9124 <srivicky050@gmail.com>
2025-09-19 01:04:52 +0530

    design enhancement in login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,6 +3,9 @@ import type { FormEvent } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import MyIcon from '../assets/UserIcon.jpg'
+import BgPic from'../assets/login-background.jpg'
+import { FaLock,FaUser } from "react-icons/fa";
 import api from '../api';
 
 const Login: React.FC = () => {
@@ -34,11 +37,12 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-gray-900">
-      
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
+    <div className="flex items-center justify-end min-h-screen bg-gray-100 text-gray-900">
+      <img src={BgPic} alt="" className=' w-200 min-h-screen'/>
+      <div className="w-full max-w-lg p-8 space-y-6 bg-green-300 rounded-lg shadow-lg min-h-screen flex flex-col  items-center justify-center">
+        <img src={MyIcon} alt="" className='h-20 rounded-full'/>
         <h2 className="text-3xl font-bold text-center">
-          Secure Login
+          Partner login
         </h2>
 
         <form onSubmit={handleSubmit} className="space-y-6">
@@ -51,10 +55,12 @@ const Login: React.FC = () => {
           <div>
             <label 
               htmlFor="username" 
-              className="block mb-2 text-sm font-medium text-gray-700"
+              className="block mb-0 text-sm font-medium text-gray-700 "
             >
               Username
             </label>
+            <div>
+              <FaUser className='absolute top-78.5 translate-y-2 left-223 '/>
             <input
               id="username"
               type="text"
@@ -62,17 +68,19 @@ const Login: React.FC = () => {
               onChange={(e) => setUsername(e.target.value)}
               required
               disabled={isLoading}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
+              className="w-l px-5 py-2  border-b border-black  placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
             />
+            </div>
           </div>
 
           <div>
             <label 
               htmlFor="password" 
-              className="block mb-2 text-sm font-medium text-gray-700"
+              className="block mb-0 text-sm font-medium text-gray-700"
             >
               Password
             </label>
+            <FaLock className='absolute top-100 translate-y-2 left-223 '/>
             <input
               id="password"
               type="password"
@@ -80,14 +88,14 @@ const Login: React.FC = () => {
               onChange={(e) => setPassword(e.target.value)}
               required
               disabled={isLoading}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
+              className="w-full px-5 py-2 border-b border-black  placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
             />
           </div>
 
           <button 
             type="submit" 
             disabled={isLoading}
-            className="w-full py-3 px-4 font-semibold text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:bg-blue-300 disabled:cursor-not-allowed transition-colors"
+            className="w-full py-3 px-4 font-semibold text-white bg-blue-600 rounded-4xl shadow-sm hover:shadow-black bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:bg-blue-300 disabled:cursor-not-allowed transition-colors"
           >
             {isLoading ? 'Logging in...' : 'Login'}
           </button>
